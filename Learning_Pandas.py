c679bb0b126277b843d1e185fd1b36eda0538a73
Hafiz Haq <muhammadkamran_16@yahoo.com>
2019-06-07 14:53:25 +0300

    Complete first pandas tutorial

--- Learning_Pandas.py
+++ Learning_Pandas.py
@@ -12,7 +12,7 @@ Created on Thu Jun  6 17:29:02 2019
 import pandas as pd
 import nympy as np
 import matplotlib.pyplot as plot
-p.set_option('max_columns', 100)
+pd.set_option('max_columns', 100)
 #matplotlib inline
  
 
@@ -66,14 +66,43 @@ print ('\n')
 print (cities[cities.isnull()])
 
 
+#################################DataFrame analysis
+
+
+data = {'year': [2010, 2011, 2012, 2013, 2014, 2015, 2012, 2013],
+        'team': ['Viking', 'Viking', 'Viking', 'Sparta', 'Sparta', 'Sparta', 'Roman', 'Roman'],
+        'Wins': [11, 8, 10, 15, 11, 6, 10, 4],
+        'loses': [5, 8, 6, 1, 5, 10, 6, 12]}
+football= pd.DataFrame(data, columns=['year', 'team', 'Wins', 'loses'])
+football
+
+
+from_csv = pd.read_csv('C:\\Users\\hhaq\\Documents\\GitHub\\Learn_Data_Science\\data\\mariano-rivera.csv')
+from_csv.head()
 
 
 
+cols = ['num', 'game', 'date', 'team', 'home_away', 'opponent',
+        'result', 'quarter', 'distance', 'receiver', 'score_before',
+        'score_after']
+no_headers = pd.read_csv('C:\\Users\\hhaq\\Documents\\GitHub\\Learn_Data_Science\\data\\peyton-passing-TDs-2012.csv', sep=',', header=None,
+                         names=cols)
+no_headers.head()
+
+football.to_excel('football.xlsx', index=False)
+
+del football
+
+football= pd.read_excel('football.xlsx', 'Sheet1')
+football
 
 
+#######################################URL connection 
 
+url = 'https://raw.github.com/gjreda/best-sandwiches/master/data/best-sandwiches-geocode.tsv'
 
-
+from_url = pd.read_table(url, sep='\t')
+from_url.head(3)
 
 
 
