8a6ee0f0663211f72e3f7cc69bdec9b326f55912
Hafiz Haq <muhammadkamran_16@yahoo.com>
2019-06-10 12:38:07 +0300

    Update Machine_learning_basics.py

--- Machine_learning_basics.py
+++ Machine_learning_basics.py
@@ -100,6 +100,68 @@ iris['PCA2'] = X_2D[:, 1]
 sns.lmplot("PCA1", "PCA2", hue='species', data=iris, fit_reg=False);
 
 
+#####################Iris clustering example 
+from sklearn.mixture import GaussianMixture           ##model selection
+model= GaussianMixture(n_components=3, covariance_type='full')   ##instantiating the hyperparameter
+model.fit(X_iris)   ###fit the model
+y_gmm= model.predict(X_iris)   ####predicting the cluster
+
+############plooting the results
+iris['cluster'] = y_gmm
+sns.lmplot("PCA1", "PCA2", hue='species', data=iris, col='cluster', fit_reg=False);
+
+
+#############Application: Character recogonition in an image 
+####Pre-formatted digits in Scikit
+from sklearn.datasets import load_digits
+digits = load_digits()
+digits.images.shape
+
+
+
+
+import matplotlib.pyplot as plt
+
+fig, axes = plt.subplots(10, 10, figsize=(8, 8),
+                         subplot_kw={'xticks':[], 'yticks':[]},
+                         gridspec_kw=dict(hspace=0.1, wspace=0.1))
+
+for i, ax in enumerate(axes.flat):
+    ax.imshow(digits.images[i], cmap='binary', interpolation='nearest')
+    ax.text(0.05, 0.05, str(digits.target[i]),
+            transform=ax.transAxes, color='green')
+
+
+X=digits.data
+X.shape
+
+y=digits.target
+y.shape
+
+
+
+###############Classification
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, random_state=0)
+from sklearn.naive_bayes import GaussianNB
+model=GaussianNB()
+model.fit(Xtrain, ytrain)
+y_model= model.predict(Xtest)
+from sklearn.metrics import accuracy_score
+accuracy_score(ytest,y_model)
+
+#############confusion matrix= where we have gone wrong 
+from sklearn.metrics import confusion_matrix
+
+mat = confusion_matrix(ytest, y_model)
+
+sns.heatmap(mat, square=True, annot=True, cbar=False)
+plt.xlabel('predicted value')
+plt.ylabel('true value');
+
+
+
+
+
 
 
 
