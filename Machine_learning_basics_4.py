70e8ecb4b9cd6d82ff37da106803c6efdd8bb579
Hafiz Haq <muhammadkamran_16@yahoo.com>
2019-06-11 10:40:04 +0300

    Update Machine_learning_basics_4.py

--- Machine_learning_basics_4.py
+++ Machine_learning_basics_4.py
@@ -33,3 +33,47 @@ plt.axis(lim);
 yprob=model.predict_proba(Xnew)
 yprob[-8:].round(2)
 
+########################Multinominal Naive Bayesian classification
+from sklearn.datasets import fetch_20newsgroups
+
+data = fetch_20newsgroups()
+data.target_names
+data
+
+
+categories = ['talk.religion.misc', 'soc.religion.christian',
+              'sci.space', 'comp.graphics']
+train = fetch_20newsgroups(subset='train', categories=categories)
+test = fetch_20newsgroups(subset='test', categories=categories)
+
+print(train.data[5])
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+model= make_pipeline(TfidfVectorizer(), MultinomialNB())
+model.fit(train.data, train.target)
+labels= model.predict(test.data)
+
+
+from sklearn.metrics import confusion_matrix
+mat= confusion_matrix(test.target, labels)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
+            xticklabels=train.target_names, yticklabels=train.target_names)
+plt.xlabel('true label')
+plt.ylabel('predicted label');
+
+
+
+#####################Simple example of single string returning prediction
+
+
+def predict_category(s, train=train, model=model):
+    pred = model.predict([s])
+    return train.target_names[pred[0]]
+
+predict_category('sending a payload to the ISS')
+predict_category('discussing Islam vs atheism')
+predict_category('discussing Buddhism vs Judiasm')
+predict_category('determining the screen resolution')
+predict_category('inches vs centimeter')
