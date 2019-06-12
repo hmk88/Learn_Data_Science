9ca1ae10cbd26c0ede02ada87b25f6a94fa1ebaf
Hafiz Haq <muhammadkamran_16@yahoo.com>
2019-06-12 13:32:23 +0300

    Update Support_Vextor_Machine(SVM)_basics.py

--- Support_Vextor_Machine(SVM)_basics.py
+++ Support_Vextor_Machine(SVM)_basics.py
@@ -44,13 +44,187 @@ from sklearn.svm import SVC  #####Support Vector Classifier
 model=SVC(kernel='linear', C=1E10)
 model.fit(X,y)
 
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    # create grid to evaluate model
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    
+    # plot decision boundary and margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+    
+    # plot support vectors
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=1, facecolors='none');
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(model);
+
+#######support vectors are the points on the graph that separate the two dataset 
+model.support_vectors_
+
+def plot_svm(N=10, ax=None):
+    X, y = make_blobs(n_samples=200, centers=2,
+                      random_state=0, cluster_std=0.60)
+    X = X[:N]
+    y = y[:N]
+    model = SVC(kernel='linear', C=1E10)
+    model.fit(X, y)
+    
+    ax = ax or plt.gca()
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+    ax.set_xlim(-1, 4)
+    ax.set_ylim(-1, 6)
+    plot_svc_decision_function(model, ax)
+
+fig, ax = plt.subplots(1, 2, figsize= (16,6))
+fig.subplots_adjust(left=0.0625, right=0.95, wspace=0.1)
+for axi, N in zip(ax, [60, 120]):
+    plot_svm(N,axi)
+    axi.set_title('N= {0}'.format(N))
 
 
+#############Interactive widgets to view feature of the SVM 
+from ipywidgets import interact, fixed
+interact(plot_svm, N=[10, 200], ax=fixed(None));
+
+############Beyond linear boundary, inseparable dataset
+from sklearn.datasets.samples_generator import make_circles
+X, y= make_circles(100, factor=.1, noise=.1)
+
+clf= SVC(kernel='linear').fit(X,y)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(clf, plot_support=False);
+
+#########Since the data cannot be separated, we will project it in a higher dimension
+r = np.exp(-(X ** 2).sum(1))
+########Using 3d plotting
+from mpl_toolkits import mplot3d
+def plot_3D(elev=30, azim=30, X=X, y=y):
+    ax = plt.subplot(projection='3d')
+    ax.scatter3D(X[:, 0], X[:, 1], r, c=y, s=50, cmap='autumn')
+    ax.view_init(elev=elev, azim=azim)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('r')
+
+interact(plot_3D, elev=[-90, 90], azip=(-180, 180),
+         X=fixed(X), y=fixed(y));
+############bad plotting from previous function 
+
+#######This kernel transform is used to turn fast linear method into fast non-linear method
+clf = SVC(kernel='rbf', C=1E6)
+clf.fit(X, y)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(clf)
+plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1],
+            s=300, lw=1, facecolors='none');
+
+            
+            
+#################Tuning the SVM: Softening margins
+######When data has some amount of overlap
+X, y = make_blobs(n_samples=100, centers=2,
+                  random_state=0, cluster_std=2)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn');
+#######In this case, SVM implementation has a bit of a fudge-factor which softens the margins
+X, y = make_blobs(n_samples=100, centers=2,
+                  random_state=0, cluster_std=0.8)
+
+fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+fig.subplots_adjust(left=0.0625, right=0.95, wspace=0.1)
+
+for axi, C in zip(ax, [10.0, 0.1]):
+    model = SVC(kernel='linear', C=C).fit(X, y)
+    axi.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+    plot_svc_decision_function(model, axi)
+    axi.scatter(model.support_vectors_[:, 0],
+                model.support_vectors_[:, 1],
+                s=300, lw=1, facecolors='none');
+    axi.set_title('C = {0:.1f}'.format(C), size=14)
 
 
+#######################Facial recogonition 
+from sklearn.datasets import fetch_lfw_people
+faces = fetch_lfw_people(min_faces_per_person=60)
+print(faces.target_names)
+print(faces.images.shape)
 
 
+fig, ax = plt.subplots(3, 5)
+for i, axi in enumerate(ax.flat):
+    axi.imshow(faces.images[i], cmap='bone')
+    axi.set(xticks=[], yticks=[],
+            xlabel=faces.target_names[faces.target[i]])
+    
+    
+#############Applying PCA for facial recogonition 
+from sklearn.svm import SVC    
+from sklearn.decomposition import PCA as RandomizedPCA
+from sklearn.pipeline import make_pipeline
+
+pca= RandomizedPCA(n_components=150, whiten=True, random_state=42)
+svc= SVC(kernel='rbf', class_weight='balanced')
+model = make_pipeline(pca, svc)
+
+#####for classifier output: separate the data into test and train
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest = train_test_split(faces.data, faces.target,
+                                                random_state=42)
 
 
+##############Here we will adjust the value of C which controls the margin hardness
+##############and gamma which controls the radial basis function kernel
+from sklearn.model_selection import GridSearchCV
+param_grid = {'svc__C': [1, 5, 10, 50],
+              'svc__gamma': [0.0001, 0.0005, 0.001, 0.005]}
+grid = GridSearchCV(model, param_grid)
+
+%time grid.fit(Xtrain, ytrain)
+print(grid.best_params_)
+
+model = grid.best_estimator_
+yfit = model.predict(Xtest)
+
+###########predicted results
+fig, ax = plt.subplots(4, 6)
+for i, axi in enumerate(ax.flat):
+    axi.imshow(Xtest[i].reshape(62, 47), cmap='bone')
+    axi.set(xticks=[], yticks=[])
+    axi.set_ylabel(faces.target_names[yfit[i]].split()[-1],
+                   color='black' if yfit[i] == ytest[i] else 'red')
+fig.suptitle('Predicted Names; Incorrect Labels in Red', size=14);
+
+###############Performance using the classification report 
+
+
+from sklearn.metrics import classification_report
+print(classification_report(ytest, yfit,
+                            target_names=faces.target_names))
+
+###########################Confusion matrix
+from sklearn.metrics import confusion_matrix
+mat = confusion_matrix(ytest, yfit)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
+            xticklabels=faces.target_names,
+            yticklabels=faces.target_names)
+plt.xlabel('true label')
+plt.ylabel('predicted label');
 
 
