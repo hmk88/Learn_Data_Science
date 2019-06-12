fe49d0b9fb6eff742dc5cdbf5befba1154079aeb
Hafiz Haq <muhammadkamran_16@yahoo.com>
2019-06-12 11:28:02 +0300

    Update Support_Vextor_Machine(SVM)_basics.py

--- Support_Vextor_Machine(SVM)_basics.py
+++ Support_Vextor_Machine(SVM)_basics.py
@@ -6,4 +6,51 @@ Created on Wed Jun 12 10:13:36 2019
 """
 
 #Source: https://jakevdp.github.io/PythonDataScienceHandbook/ 
+######################################SVM
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns; sns.set()
+
+from sklearn.datasets.samples_generator import make_blobs
+X, y = make_blobs(n_samples=50, centers=2,
+                  random_state=0, cluster_std=0.60)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn');
+xfit=np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plt.plot([0.6], [2.1], 'x', color='red', markeredgewidth=2, markersize=10)
+
+for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
+    plt.plot(xfit, m*xfit + b, '-k')
+plt.xlim(-1, 3.5)
+
+######SVM way of plotting
+xfit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0,2, 2.9, 0.2)]:
+    yfit= m*xfit + b
+    plt.plot(xfit, yfit, '-k')
+    plt.fill_between(xfit, yfit-d, yfit+d, edgecolor= 'none',
+                     color='#AAAAAA', alpha=0.4)
+                     
+plt.xlim(-1, 3.5)
+
+
+
+
+####################Fitting SVM
+from sklearn.svm import SVC  #####Support Vector Classifier
+model=SVC(kernel='linear', C=1E10)
+model.fit(X,y)
+
+
+
+
+
+
+
+
+
+
 
